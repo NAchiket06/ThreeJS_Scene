116a08ee84b9b6f149cb08aa998c0036b002093b
Nachiket Jadhav <44014957+NAchiket06@users.noreply.github.com>
2022-07-30 21:15:34 +0530

    Base Room Setup

--- sources/Experience/World.js
+++ sources/Experience/World.js
@@ -14,22 +14,35 @@ export default class World
         {
             if(_group.name === 'base')
             {
-                this.setDummy()
+                // this.setDummy()
+                this.setRoom()
             }
         })
     }
 
-    setDummy()
-    {
-        this.resources.items.lennaTexture.encoding = THREE.sRGBEncoding
+    // setDummy()
+    // {
+    //     this.resources.items.lennaTexture.encoding = THREE.sRGBEncoding
         
-        const cube = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshBasicMaterial({ map: this.resources.items.lennaTexture })
-        )
-        this.scene.add(cube)        
-    }
+    //     const cube = new THREE.Mesh(
+    //         new THREE.BoxGeometry(1, 1, 1),
+    //         new THREE.MeshBasicMaterial({ map: this.resources.items.lennaTexture })
+    //     )
+    //     this.scene.add(cube)        
+    // }
 
+    setRoom()
+    {
+        this.room = {}
+        this.room.model = this.resources.items.roomModel.scene
+
+        this.scene.add(this.room.model)
+
+        const directionalLight = new THREE.DirectionalLight('#ffffff',5)
+        directionalLight.position.set(10,10,10)
+
+        this.scene.add(directionalLight)
+    }
     resize()
     {
     }
